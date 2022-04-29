1dddda6e115574d0b0261079da4393953b688dcb
Alyssa Jackson <alyssariah@alyssas-mbp.lan>
2022-04-29 16:12:43 -0400

    new testing

---
+++ tests/functional/LogInCest.php
@@ -0,0 +1,14 @@
+<?php
+
+class LogInCest
+{
+    public function _before(FunctionalTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(FunctionalTester $I)
+    {
+        $I->amGoingTo('start in the backend');
+    }
+}
